a888ef321dff65a16052758d1b6982c894731694
Anish Ranjan <137291124+a85nishranjan@users.noreply.github.com>
2023-08-15 05:03:17 +0530

    Add files via upload

---
+++ Clang.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+   int a [2][3]={1,2,3,4,5,6};
+   // printf("%u",&a);
+
+
+   /*int i,*ptr,n;
+   scanf("%d",&n);
+   ptr=malloc(n*sizeof(int));
+   for ( i = 0; i < n; i++)
+   {
+    scanf("%d",ptr+i);
+   }
+ int *fall= realloc(ptr,3*sizeof(int));
+    for ( i = 0; i < 3; i++)
+   {
+    printf("%d\n",ptr+i);
+   }*/
+
+   char name[]="pankaj";
+  // printf("%s",name+1);
+   printf("%u",&a[1]);
+
+
+
+
+   
+
+}
+//unsigned short int a=-3;
+
+//printf("%d",a); 
+
+
+//}
+  // int mult(int x, int y);
+
+/*void main(){
+//int a,b,c;
+//a=b=c=20;
+//5515;
+//1121.32;
+ 
+//scanf("%d %d",&a,&b);
+//printf("the sum of %d and %d is %d ",a,b,c);
+//int i=1516565;
+//for( ; i>0;i=i/10){
+//printf("%d\n",i%10);
+//continue;
+//printf("kjnwkjdm");
+//}
+/*int a=10,b=20,c;
+
+c =mult(a,b);
+printf("%d",c);
+}
+
+int mult(int x, int y){
+   return x*y;
+
+
+*/
+/*
+int j=100;
+
+int *p=(int*)10;
+
+
+
+printf("%d",*p);
+*/
+
+
